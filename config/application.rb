c15c0e69e3b46466ae392b9c2bb8378c619d9d11
Jaewon Bae <jaey.pro@gmail.com>
2024-01-12 12:38:29 -0500

    add assets precompile to application.rb config

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module RailsAdminExperiment
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
 
     # Configuration for the application, engines, and railties goes here.
     #
